00338f78d9f016c0414d744e381119a8961713e1
Michael Yuan <michael@secondstate.io>
2023-05-08 01:42:52 +0000

    Optimize logging
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- src/github-pr-review.rs
+++ src/github-pr-review.rs
@@ -25,7 +25,7 @@ static MODEL : ChatModel = ChatModel::GPT4;
 pub async fn run() -> anyhow::Result<()> {
     dotenv().ok();
     logger::init();
-    log::info!("Running pr-review/playground");
+    log::debug!("Running github-pr-review/playground");
 
     let login = env::var("github_login").unwrap_or("juntao".to_string());
     let owner = env::var("github_owner").unwrap_or("flows-network".to_string());
@@ -51,12 +51,12 @@ async fn handler(
     trigger_phrase: &str,
     payload: EventPayload,
 ) {
-    log::debug!("Received payload: {:?}", payload);
+    // log::debug!("Received payload: {:?}", payload);
     let (pull_url, issue_number, _contributor) = match payload {
         EventPayload::IssuesEvent(e) => {
             if e.action != IssuesEventAction::Opened {
                 // Only responds to newly opened issues
-                log::info!("Received an ignorable event for issues.");
+                log::debug!("Received an issue event that is NOT Opened. Ignore");
                 return;
             }
             (e.issue.title, e.issue.number, e.issue.user.login)
@@ -64,9 +64,10 @@ async fn handler(
 
         EventPayload::IssueCommentEvent(e) => {
             if e.action == IssueCommentEventAction::Deleted {
-                log::info!("Deleted issue event");
+                log::debug!("Deleted issue comment");
                 return;
             }
+            log::debug!("Other event for issue comment");
 
             let body = e.comment.body.unwrap_or_default();
 
@@ -149,6 +150,7 @@ async fn handler(
                 resp.push_str(f.blob_url.as_str());
                 resp.push_str(")\n\n");
 
+                log::debug!("Sending file to OpenAI: {}", filename);
                 let co = ChatOptions {
                     model: MODEL,
                     restart: true,
@@ -160,12 +162,14 @@ async fn handler(
                     Ok(r) => {
                         resp.push_str(&r.choice);
                         resp.push_str("\n\n");
+                        log::debug!("Received OpenAI resp for file: {}", filename);
                     }
                     Err(e) => {
                         log::error!("OpenAI returns error for file review for {}: {}", filename, e);
                     }
                 }
 
+                log::debug!("Sending patch to OpenAI: {}", filename);
                 let co = ChatOptions {
                     model: MODEL,
                     restart: false,
@@ -179,9 +183,10 @@ async fn handler(
                     Ok(r) => {
                         resp.push_str(&r.choice);
                         resp.push_str("\n\n");
+                        log::debug!("Received OpenAI resp for patch: {}", filename);
                     }
                     Err(e) => {
-                        log::error!("OpenAI returns error for file review for {}: {}", filename, e);
+                        log::error!("OpenAI returns error for patch review for {}: {}", filename, e);
                     }
                 }
             }
